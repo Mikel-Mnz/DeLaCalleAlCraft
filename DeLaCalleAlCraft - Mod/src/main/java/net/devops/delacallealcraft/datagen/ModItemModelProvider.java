6219e9d80ced324c3392be9b62860f49b1955923
Miguel Mnz <mikelo.mnz05@gmail.com>
2025-05-14 16:00:02 -0600

    Se configuro el DataGen para la creacion automatica de los JOSN de texturas, modelos, interacciones y recetas. Se eliminaron los que ya existian para evitar errores de ejecucion.

---
+++ DeLaCalleAlCraft - Mod/src/main/java/net/devops/delacallealcraft/datagen/ModItemModelProvider.java
@@ -0,0 +1,26 @@
+package net.devops.delacallealcraft.datagen;
+
+import net.devops.delacallealcraft.DeLaCalleAlCraft;
+import net.devops.delacallealcraft.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, DeLaCalleAlCraft.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.HARINA.get());
+        basicItem(ModItems.TORTILLA.get());
+        basicItem(ModItems.TACO_DE_SAL.get());
+        basicItem(ModItems.SAL.get());
+        basicItem(ModItems.TACO_AL_PASTOR.get());
+        basicItem(ModItems.ACHIOTE.get());
+        basicItem(ModItems.CARNE_CONDIMENTADA.get());
+        basicItem(ModItems.CARNE_CONDIMENTADA_CRUDA.get());
+        basicItem(ModItems.TROMPO_CRUDO.get());
+    }
+}
